d40d4999e3b7de164fd4d6b10b4af903676114b3
Hamza COŞKUN <hamzacoskun41@gmail.com>
2025-07-22 21:02:55 +0300

    optimise keyboard packets, handle keyboard inputs fluently, do not register duplicated event listeners

--- Frontend/src/pages/game-section/eventListeners.ts
+++ Frontend/src/pages/game-section/eventListeners.ts
@@ -4,60 +4,84 @@ import { GameEventBus } from "./gameEventBus";
 import { GameInfo } from "./network";
 import { WebSocketClient } from "./wsclient";
 
-function listenNonLocalInputs() {
-	window.addEventListener("keydown", (event) => {
-		let moved = false;
+export class KeyboardInputHandler {
+	private direction: "up" | "down" | "stop" = "stop";
+	private side: "left" | "right" = "left";
+	private mode: "local" | "remote" | "unset" = "unset";
 
-		if (event.key.toLowerCase() === 'w') {
-			WebSocketClient.getInstance().emit("player-move", { direction: "up" });
-			moved = true;
+	private static instance: KeyboardInputHandler;
+
+	public static getInstance(): KeyboardInputHandler {
+		if (!KeyboardInputHandler.instance) {
+			KeyboardInputHandler.instance = new KeyboardInputHandler();
 		}
-		else if (event.key.toLowerCase() === 's') {
-			WebSocketClient.getInstance().emit("player-move", { direction: "down" });
-			moved = true;
+		return KeyboardInputHandler.instance;
+	}
+
+	public setMode(mode: "local" | "remote"): void {
+		this.mode = mode;
+	}
+
+	public keyToDirectionAndSide(key: string): { direction: "up" | "down" | "stop", side: "left" | "right" } | null {
+		switch (key.toLowerCase()) {
+			case 'w':
+				return { direction: "up", side: "left" };
+			case 'arrowup':
+				return { direction: "up", side: "right" };
+			case 's':
+				return { direction: "down", side: "left" };
+			case 'arrowdown':
+				return { direction: "down", side: "right" };
+			default:
+				return null;
 		}
+	}
 
-		if (moved)
-			event.preventDefault();
-		});
+	private sendStateUpdate(): void {
+		if (this.mode == "local") {
+			WebSocketClient.getInstance().emit("local-input", { direction: this.direction, player_side: this.side });
+		} else if (this.mode == "remote") {
+			WebSocketClient.getInstance().emit("player-move", { direction: this.direction });
+		} else {
+			throw new Error("KeyboardInputHandler mode is not set. Please set it to 'local' or 'remote'.");
+		}
+	}
+		
+	public keyDown(key: string): void {
+		const { direction, side } = this.keyToDirectionAndSide(key)!;
+		if (this.direction === direction && (this.side === side || this.mode === "remote"))
+			return;
 
-		window.addEventListener("keyup", (e) => {
-		if (["w", "s"].includes(e.key.toLowerCase()))
-			WebSocketClient.getInstance().emit("player-move", { direction: "stop" });
-		});
+		this.direction = direction;
+		this.side = side;
+		this.sendStateUpdate();
+	}
+
+	public keyUp(key: string): void {
+		const { direction, side } = this.keyToDirectionAndSide(key)!;
+		if (this.direction !== direction && (this.side === side || this.mode === "remote")) // old direction is not the same as current, it means there is override.
+			return;
+
+		this.direction = "stop";
+		this.sendStateUpdate();
+	}
+
+	public listen() {
+		window.removeEventListener("keydown", onKeyDown);
+		window.removeEventListener("keyup", onKeyUp);
+		window.addEventListener("keydown", onKeyDown);
+		window.addEventListener("keyup", onKeyUp);
+	}
 }
 
-function listenLocalInputs() {
-	window.addEventListener("keydown", (event) => {
-	let moved = false;
+function onKeyDown(event: KeyboardEvent) {
+	event.preventDefault();
+	KeyboardInputHandler.getInstance().keyDown(event.key);
+}
 
-	if (event.key.toLowerCase() === 'w') {
-		WebSocketClient.getInstance().emit("local-input", { player_side: "left", direction: "up" });
-		moved = true;
-	} else if (event.key.toLowerCase() === 's') {
-		WebSocketClient.getInstance().emit("local-input", { player_side: "left", direction: "down" });
-		moved = true;
-	}
-
-	if (event.key === 'ArrowUp') {
-		WebSocketClient.getInstance().emit("local-input", { player_side: "right", direction: "up" });
-		moved = true;
-	} else if (event.key === 'ArrowDown') {
-		WebSocketClient.getInstance().emit("local-input", { player_side: "right", direction: "down" });
-		moved = true;
-	}
-
-	if (moved)
-		event.preventDefault();
-	});
-
-	window.addEventListener("keyup", (e) => {
-	if (["w", "s"].includes(e.key.toLowerCase()))
-		WebSocketClient.getInstance().emit("local-input", { player_side: "left", direction: "stop" });
-
-	if (["ArrowUp", "ArrowDown"].includes(e.key))
-		WebSocketClient.getInstance().emit("local-input", { player_side: "right", direction: "stop" });
-	});
+function onKeyUp(event: KeyboardEvent) {
+	event.preventDefault();
+	KeyboardInputHandler.getInstance().keyUp(event.key);
 }
 
 function listenPauseInputs(gameInfo: GameInfo) {
@@ -95,10 +119,11 @@ function listenPauseInputs(gameInfo: GameInfo) {
 
 export function listenPlayerInputs(gameInfo: GameInfo) {
 	if (gameInfo.mode === 'remoteGame' || gameInfo.mode === 'vsAI' || gameInfo.mode === 'tournament') {
-		listenNonLocalInputs();
+		KeyboardInputHandler.getInstance().setMode("remote");
 	} else if (gameInfo.mode === 'localGame') {
-		listenLocalInputs();
+		KeyboardInputHandler.getInstance().setMode("local");
 	}
+	KeyboardInputHandler.getInstance().listen();
 
 	const resumeButton = document.getElementById("resume-button") as HTMLButtonElement;
 	if (gameInfo.mode !== 'remoteGame' && gameInfo.mode !== 'tournament') {
