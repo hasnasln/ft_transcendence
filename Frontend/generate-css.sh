fc12f4101953fb50de0b81608a91194df2dcde52
facetint <fatmanurcetintas7@gmail.com>
2025-08-13 05:34:21 +0300

    build: correct Play page TS file path in Tailwind build configuration

--- Frontend/generate-css.sh
+++ Frontend/generate-css.sh
@@ -1,7 +1,7 @@
 rm src/styles/*
 PAGE_GLOBS="src/pages/home.ts,src/pages/Settings.ts,src/pages/profile.ts" npx tailwindcss -c tailwind.page.config.js -i src/style.css -o src/styles/home.css --minify
 PAGE_GLOBS="src/pages/game.ts,src/pages/game-section/*.ts" npx tailwindcss -c tailwind.page.config.js -i src/style.css -o src/styles/game.css --minify
-PAGE_GLOBS="src/pages/play.ts,src/pages/play-page.ts," npx tailwindcss -c tailwind.page.config.js -i src/style.css -o src/styles/play.css --minify
+PAGE_GLOBS="src/pages/play.ts,src/pages/PlayPage.ts," npx tailwindcss -c tailwind.page.config.js -i src/style.css -o src/styles/play.css --minify
 PAGE_GLOBS="src/pages/login.ts" npx tailwindcss -c tailwind.page.config.js -i src/style.css -o src/styles/login.css --minify
 PAGE_GLOBS="src/pages/register.ts" npx tailwindcss -c tailwind.page.config.js -i src/style.css -o src/styles/register.css --minify
 PAGE_GLOBS="src/pages/ServerErrorPage.ts" npx tailwindcss -c tailwind.page.config.js -i src/style.css -o src/styles/500.css --minify
