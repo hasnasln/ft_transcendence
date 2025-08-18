b20e726227d3d7681cacd93cc5e7e229e14276c7
haslan <hasnasln0207@gmail.com>
2025-08-18 10:50:04 +0300

    turnuva ağacı gerçeğe alındı, game server locale çekildi

--- Frontend/src/pages/play.ts
+++ Frontend/src/pages/play.ts
@@ -173,7 +173,7 @@ export class GameManager {
 		this.uiManager.cacheDOMElements();
 		GamePage.enablePage();
 		GameEventBus.getInstance().emit({ type: 'CONNECTING_TO_SERVER' });
-		WebSocketClient.getInstance().connect("http://game.transendence.com:3001")
+		WebSocketClient.getInstance().connect("localhost:3001")
 			.catch(err => {
 				GameEventBus.getInstance().emit({ type: 'CONNECTING_TO_SERVER_FAILED', payload: { error: err } });
 				throw new Error("WebSocket connection failed.");
